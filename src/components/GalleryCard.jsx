7151417ce8b3091b97f474bbb1a0a3daa4790640
Daniel Nelson <lukannelson@gmail.com>
2024-04-01 21:09:00 -0400

    v22

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -1,9 +1,59 @@
+// import React, { useState } from 'react';
+// import { Link, useLocation } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faImages } from '@fortawesome/free-solid-svg-icons'; // Import the faImages icon
 
+// const GalleryCard = ({ product, currentPage, showViolentContent }) => {
+//   const location = useLocation();
+//   const searchParams = new URLSearchParams(location.search);
+//   const page = searchParams.get('page');
+//   const hasVideo = product.image.some((item) => item.includes('.mp4'));
+//   const [isHovered, setIsHovered] = useState(false);
+
+//   const handleHover = () => {
+//     setIsHovered(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setIsHovered(false);
+//   };
+
+//   return (
+//     <div className="gallery-card">
+//       <Link to={`${product.id}?page=${currentPage}`} className="link-no-underline">
+//         {showViolentContent || !product.hasViolence ? (
+//           hasVideo ? (
+//             <video className="gallery-video" autoPlay width="auto" loop muted={!isHovered} onMouseOver={handleHover} onMouseLeave={handleMouseLeave} playsInline controls={false}>
+//               <source src={product.image[0]} type="video/mp4" />
+//               Your browser does not support the video tag.
+//             </video>
+//           ) : (
+//             <img src={product.image[0]} alt={product.name} className="gallery-image" />
+//           )
+//         ) : (
+//           // Display a message or placeholder for violent content when showViolentContent is false
+//           <div className="restricted-content-container">
+//             {/* <h3> Warning! </h3> */}
+//             <div> 
+//             <video className="warning-image" autoPlay muted width="200px" loop playsInline controls={false}>
+//             <source src='/videos/toxic.mp4' type="video/mp4" />
+//             Your browser does not support the video tag.
+//           </video>
+//           </div>
+//            <p>Content Warning: This piece may contain sensitive or explicit material. Proceed with caution. To view, click the button with the crossed-out eye.</p>
+//           </div>
+//         )}
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default GalleryCard;
 
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImages } from '@fortawesome/free-solid-svg-icons'; // Import the faImages icon
+import { faImages } from '@fortawesome/free-solid-svg-icons';
 
 const GalleryCard = ({ product, currentPage, showViolentContent }) => {
   const location = useLocation();
@@ -24,25 +74,38 @@ const GalleryCard = ({ product, currentPage, showViolentContent }) => {
     <div className="gallery-card">
       <Link to={`${product.id}?page=${currentPage}`} className="link-no-underline">
         {showViolentContent || !product.hasViolence ? (
-          hasVideo ? (
-            <video className="gallery-video" autoPlay width="auto" loop muted={!isHovered} onMouseOver={handleHover} onMouseLeave={handleMouseLeave} playsInline controls={false}>
-              <source src={product.image[0]} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          ) : (
-            <img src={product.image[0]} alt={product.name} className="gallery-image" />
-          )
+          <>
+            {/* {product.image.length > 1 && (
+              <FontAwesomeIcon icon={faImages} className="image-icon" />
+            )} */}
+            {hasVideo ? (
+              <video
+                className="gallery-video"
+                autoPlay
+                width="auto"
+                loop
+                muted={!isHovered}
+                onMouseOver={handleHover}
+                onMouseLeave={handleMouseLeave}
+                playsInline
+                controls={false}
+              >
+                <source src={product.image[0]} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            ) : (
+              <img src={product.image[0]} alt={product.name} className="gallery-image" />
+            )}
+          </>
         ) : (
-          // Display a message or placeholder for violent content when showViolentContent is false
           <div className="restricted-content-container">
-            {/* <h3> Warning! </h3> */}
-            <div> 
-            <video className="warning-image" autoPlay muted width="200px" loop playsInline controls={false}>
-            <source src='/videos/toxic.mp4' type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          </div>
-           <p>Content Warning: This piece may contain sensitive or explicit material. Proceed with caution. To view, click the button with the crossed-out eye.</p>
+            <div>
+              <video className="warning-image" autoPlay muted width="200px" loop playsInline controls={false}>
+                <source src='/videos/toxic.mp4' type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <p>Content Warning: This piece may contain sensitive or explicit material. Proceed with caution. To view, click the button with the crossed-out eye.</p>
           </div>
         )}
       </Link>
